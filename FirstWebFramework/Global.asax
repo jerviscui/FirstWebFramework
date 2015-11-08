e739934d1f7fad33990b51bb2ab2555a8e8f758d
Jervis <cuijervis@126.com>
2015-11-08 11:07:15 +0800

    create

---
+++ FirstWebFramework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstWebFramework.MvcApplication" Language="C#" %>
---
+++ FirstWebFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstWebFramework.Startup))]
+namespace FirstWebFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
