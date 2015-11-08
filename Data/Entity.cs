d7fd8242da4b2bf919281b07c5f0fffe6fb4e9a5
Jervis <cuijervis@126.com>
2015-11-08 13:26:34 +0800

    create

---
+++ Data/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Sex { get; set; }
+    }
+}
--- FirstWebFramework/Controllers/HomeController.cs
+++ FirstWebFramework/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Xml.Serialization;
+using Data;
 
 namespace FirstWebFramework.Controllers
 {
