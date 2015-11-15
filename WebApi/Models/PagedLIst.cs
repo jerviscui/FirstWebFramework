7410b29bea266c91117e311436f1d08e3d413861
Jervis <cuijervis@126.com>
2015-11-15 23:38:32 +0800

    add Web Api

---
+++ WebApi/Models/PagedLIst.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace WebApi.Models
+{
+    [DataContract]
+    public class PagedList<T> : List<T>, IPagedList
+    {
+        public PagedList()
+        {
+            this.AddRange(new List<T>());
+            PageIndex = 0;
+        }
+
+        public PagedList(IQueryable<T> query, int pageIndex, int pageSize)
+        {
+            if (pageSize < 1)
+            {
+                throw new ArgumentException();
+            }
+
+            TotalCount = query.Count();
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            TotalPages = (TotalCount + pageSize - 1) / pageSize;
+            this.AddRange(query.Skip(pageIndex * pageSize).Take(pageSize));
+
+            HasPreviousPage = PageIndex > 0;
+            HasNextPage = PageIndex + 1 < TotalPages;
+        }
+
+        [DataMember]
+        public int PageIndex { get; set; }
+
+        [DataMember]
+        public int PageSize { get; set; }
+        
+        [DataMember]
+        public int TotalCount { get; private set; }
+
+        [DataMember(Order = 1)]
+        public int TotalPages { get; private set; }
+
+        [DataMember(Order = 10)]
+        public bool HasPreviousPage { get; private set; }
+
+        [DataMember(Order = 10)]
+        public bool HasNextPage { get; private set; }
+    }
+}
---
+++ WebApi/Models/SerialiazePagedList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace WebApi.Models
+{
+    [DataContract]
+    public class SerialiazePagedList<T> : IPagedList
+    {
+        public SerialiazePagedList()
+        {
+            Items = new T[] {};
+        }
+
+        public SerialiazePagedList(PagedList<T> list)
+        {
+            Items = list.GetRange(0, list.Count).ToArray();
+            PageIndex = list.PageIndex;
+        }
+
+        [DataMember]
+        public T[] Items { get; set; }
+
+        [DataMember]
+        public int PageIndex { get; set; }
+
+        [DataMember]
+        public int PageSize { get; set; }
+
+        [DataMember]
+        public int TotalCount { get; private set; }
+
+        [DataMember(Order = 1)]
+        public int TotalPages { get; private set; }
+
+        [DataMember(Order = 10)]
+        public bool HasPreviousPage { get; private set; }
+
+        [DataMember(Order = 10)]
+        public bool HasNextPage { get; private set; }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+    }
+}
